a392ead0083c73f8dc36ba34e32828019a765d98
Mila Kolasava <kolasava.dev@yandex.by>
2022-05-04 23:30:55 +0200

    refactor: add js code close burger-menu when you click not links

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -2,6 +2,13 @@
 
 const headerBurger = document.querySelector(".header__burger");
 const headerNavigation = document.querySelector(".header-navigation");
+
+const closeMenu = () => {
+  document.body.classList.remove("_lock");
+  headerBurger.classList.remove("_active");
+  headerNavigation.classList.remove("_active");
+};
+
 if (headerBurger) {
   headerBurger.addEventListener("click", function (event) {
     document.body.classList.toggle("_lock");
@@ -10,30 +17,24 @@ if (headerBurger) {
   });
 }
 const menuLinks = document.querySelectorAll(".list-navigation-item[data-goto]");
-//массив ссылок
-if (menuLinks.length > 0) {
-  // проверяется пустой массив или нет. Проверка на пустой массив
+const isNotEmptyMenuLinks = menuLinks.length > 0;
+
+if (isNotEmptyMenuLinks) {
   menuLinks.forEach((menuLink) => {
     menuLink.addEventListener("click", onMenuLinkClick);
   });
 
   function onMenuLinkClick(event) {
-    console.log(event);
     const menuLink = event.target;
     if (
       menuLink.dataset.goto &&
       document.querySelector(menuLink.dataset.goto)
-      //происзодит проверка есть ли у элемента в target свойство dataset со значением goto
     ) {
       const gotoBlock = document.querySelector(menuLink.dataset.goto);
       const gotoBlockValue = gotoBlock.getBoundingClientRect().top;
-      //pageYOffset -
-      //document.querySelector(".content-header").offsetHeight;
 
       if (headerBurger.classList.contains("_active")) {
-        document.body.classList.remove("_lock");
-        headerBurger.classList.remove("_active");
-        headerNavigation.classList.remove("_active");
+        closeMenu();
       }
 
       window.scrollTo({
@@ -44,3 +45,9 @@ if (menuLinks.length > 0) {
     }
   }
 }
+
+headerNavigation.addEventListener("click", (event) => {
+  if (headerNavigation.classList.contains("_active")) {
+    closeMenu();
+  }
+});
